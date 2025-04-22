a49aa2b98fc4505ca800ce94a52c6ff6631b0d38
S7R31CH <s7r31ch@gmail.com>
2025-04-22 14:32:21 +0100

    新增项目环境初始化脚本；新增数据集说明

---
+++ initialization.ps1
@@ -0,0 +1,2 @@
+conda env create -f src/resouirces/environment.yml
+pip install src\resources\devtoolkit-0.1.0.2025032502-py3-none-any.whl
---
+++ src/resources/README.md
@@ -0,0 +1,2 @@
+## 数据集使用说明
+helldivers2-audio-assistant 项目的训练集，将 [**dat**](dat) 文件夹放在 **resource** 文件夹下即可
