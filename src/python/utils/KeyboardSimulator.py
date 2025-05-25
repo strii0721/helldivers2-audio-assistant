43202da638e2fafc8705161c20f758e40efc3308
strii0721 <strii0721@outlook.com>
2025-05-25 15:18:24 +0100

    优化键盘输入速度

--- src/python/utils/KeyboardSimulator.py
+++ src/python/utils/KeyboardSimulator.py
@@ -1,12 +1,15 @@
 from pynput.keyboard import Key, Controller
+from pynput.mouse import Button
+from pynput.mouse import Controller as MouseController
 import time
 
 class KeyboardSimulator:
     
-    KEY_PRESS_INTERVAL = 0.04
+    KEY_PRESS_INTERVAL = 0.05
     
     def __init__(self):
         self.controller = Controller()
+        self.mouse_controller =  MouseController()
     
     def read_cmd_seq(self,cmd_seq):
         for cmd in cmd_seq:
@@ -34,8 +37,10 @@ class KeyboardSimulator:
     
     def start(self):
         self.controller.press(Key.ctrl)
+        # self.mouse_controller.press(Button.button4)
         time.sleep(self.KEY_PRESS_INTERVAL)
     
     def end(self):
         self.controller.release(Key.ctrl)
+        # self.mouse_controller.release(Button.button4)
         time.sleep(self.KEY_PRESS_INTERVAL)
